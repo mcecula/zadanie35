8e4b50faf35e832269ee193ade999a8caaa66137
mcecula <mc23051988@gmail.com>
2023-03-06 21:12:55 +0100

    zadanie35

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let n = 9
+
+
+if (  100 <= n <= 200) {
+    console.log('Liczba znajduje się w przedziale');
+}
+else if (100 > n > 200) {
+    console.log('Liczba nie znajduje się w przedziale');
+}
+
+

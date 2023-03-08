839e9bdff0b0bc49f35d96eb5d92fb05aec6cd90
mcecula <mc23051988@gmail.com>
2023-03-08 20:32:02 +0100

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-let n = 9
-
-
-if (  100 <= n <= 200) {
+function num (n){
+if (100 <= n || n <= 200) {
     console.log('Liczba znajduje się w przedziale');
 }
-else if (100 > n > 200) {
+else if (100 > n || n > 200) {
     console.log('Liczba nie znajduje się w przedziale');
 }
+}
 
-
+num (120)
